cc8dfb0cc397d8a76608f1e237fc950cabccf135
Nyande <nyande20@gmail.com>
2019-03-11 19:25:54 +0300

    Initial Commit

---
+++ GetAplot/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  GetAplot
+//
+//  Created by K2 Paul on 11/03/2019.
+//  Copyright © 2019 K2 Paul. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ GetAplot/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  GetAplot
+//
+//  Created by K2 Paul on 11/03/2019.
+//  Copyright © 2019 K2 Paul. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
