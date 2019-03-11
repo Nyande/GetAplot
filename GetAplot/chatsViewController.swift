b6aa39b1ba9cee828cc88e4cb97d46dee57e5e3f
Nyande <nyande20@gmail.com>
2019-03-11 20:46:29 +0300

    First commit

---
+++ GetAplot/chatsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  chatsViewController.swift
+//  GetAplot
+//
+//  Created by K2 Paul on 11/03/2019.
+//  Copyright © 2019 K2 Paul. All rights reserved.
+//
+
+import UIKit
+
+class chatsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
